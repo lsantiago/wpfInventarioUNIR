15ffb4b14b27eb7aacf9b94664accbcde2fabfc4
lsantiago <lsquinones@gmail.com>
2017-05-11 12:28:34 -0500

    Nuevas opciones ventana inventario

---
+++ Resumen.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpfInventarioUNIR
+{
+    /// <summary>
+    /// Lógica de interacción para Resumen.xaml
+    /// </summary>
+    public partial class Resumen : Window
+    {
+        public Resumen()
+        {
+            InitializeComponent();
+        }
+    }
+}
